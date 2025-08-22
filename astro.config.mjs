b31ba72894f27b978adcbfd7c8f6b483d130643d
Tomas Rodríguez <trodrigueza@unal.edu.co>
2025-08-22 01:01:30 -0500

    testing posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +12,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [mdx()],
+  integrations: [mdx({ remarkPlugins: [remarkMath], rehypePlugins: [rehypeKatex] })],
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,4 @@
+module.exports = {
+  content: ["./src/**/*.{astro,md,mdx,html,js,ts,jsx,tsx}"],
+  plugins: [require("@tailwindcss/typography")],
+};
