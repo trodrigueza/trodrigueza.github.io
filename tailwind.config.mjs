58c7425a6b054f351e062a44858eb07fd45df240
Tomas Rodríguez <trodrigueza@unal.edu.co>
2026-05-03 01:21:31 -0500

    fixed button colors bug

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 module.exports = {
   content: ["./src/**/*.{astro,md,mdx,html,js,ts,jsx,tsx}"],
+  darkMode: ['selector', '[data-theme="dark"]'],
   plugins: [require("@tailwindcss/typography")],
 };
