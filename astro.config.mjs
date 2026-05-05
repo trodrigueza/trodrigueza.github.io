11a8e99e07e5906e46ac9ac1bb3b3f94349efbf4
Tomas Rodríguez <trodrigueza@unal.edu.co>
2026-05-05 14:50:05 -0500

    Copy button settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import { rehypeCodeBlock } from './rehype-code-block.mjs';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,25 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [mdx({ remarkPlugins: [remarkMath], rehypePlugins: [rehypeKatex] })],
+  integrations: [mdx({ 
+    remarkPlugins: [remarkMath], 
+    rehypePlugins: [rehypeKatex, rehypeCodeBlock],
+    shikiConfig: {
+      themes: {
+        light: 'catppuccin-latte',
+        dark: 'github-dark-dimmed',
+      },
+      wrap: true,
+    },
+  })],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      wrap: true,
+    },
+    rehypePlugins: [rehypeCodeBlock],
+  },
 });
